5824e86bf080cd82cf668f02c9ebed5f988c3607
Teodor Radi <radoi.teodor.cristian@gmail.com>
2024-04-21 05:31:28 -0700

    Started TeamServer

---
+++ TeamServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Text;
+
+namespace TeamServer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8001/");
+
+            listener.Start();
+
+            Console.WriteLine("Listening on port 8001...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest req = context.Request;
+
+                Console.WriteLine($"Received request for {req.Url}");
+
+                using HttpListenerResponse resp = context.Response;
+                resp.Headers.Set("Content-Type", "text/plain");
+
+                string data = "Hello there!";
+                byte[] buffer = Encoding.UTF8.GetBytes(data);
+                resp.ContentLength64 = buffer.Length;
+
+                using Stream ros = resp.OutputStream;
+                ros.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
